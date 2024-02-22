fe4173d7d4d74302e64a01ef01d05f5b05203077
valkyrie_pilot <valk@randomairborne.dev>
2024-02-21 18:00:19 -0700

    download saner files

--- gen-test-assets.sh
+++ gen-test-assets.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 mkdir ./public
 cd ./public/
-for ((i = 0 ; i < 20 ; i++)); do
+for ((i = 0 ; i < 10 ; i++)); do
   echo "Downloading image ${i}"
   curl -JOL https://picsum.photos/2048/2048 2> /dev/null &
+done
+for ((i = 0 ; i < 10 ; i++)); do
+  echo "Downloading image ${i}"
+  curl -JOL https://picsum.photos/256/256 2> /dev/null &
 done
 wait
 echo "Downloaded all images"
